8c8db3410e11157cff1a6cbf4793fadd94021dae
Harini C <157122364+Harinichith@users.noreply.github.com>
2025-02-02 12:56:47 +0530

    Delete src directory

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from '../slices/userslice';
-
-const store=configureStore({
-    reducer:{
-        userinfo:userReducer,
-    },
-});
-export default store;
--- src/granted.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
- const Granted =()=>{
-    return(
-        <div>
-        <img src="thumpsup.jpeg" width={500} alt="Granted"/>
-        </div>
-    )
- }
- export default Granted
